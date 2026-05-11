995c281d03c5c42cce06618256fc09be2f8d5543
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-11 12:48:45 +0530

    buttton tag

---
+++ Day3/index.js
@@ -0,0 +1,7 @@
+console.log("kyaa");
+let btn = document.querySelector("button");
+let body = document.querySelector("body");
+
+btn.addEventListener("click", function () {
+  body.style.backgroundColor = "red";
+})
