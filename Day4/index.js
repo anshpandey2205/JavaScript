42676678a93dd3ba5041beee04f516ac296af916
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-12 11:13:24 +0530

    print

--- Day4/index.js
+++ Day4/index.js
@@ -1,5 +1,13 @@
 let inp=document.querySelector("input");
 let h2=document.querySelector("h2");
+let btn=document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    console.log("clicked");
+    let data=inp.value;
+    h2.innerText=data;
+    inp.value="";
+})
 inp.addEventListener("input",function(e){
     h2.innerText=e.target.value;
 })
