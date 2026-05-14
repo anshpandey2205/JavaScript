c5b48d78748be2bc2ede6374009e814d9e7e6fc1
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-14 11:25:26 +0530

    promise

--- Day6/promisis.js
+++ Day6/promisis.js
@@ -11,26 +11,40 @@
 // })
 // console.log(promise)
 
-function step1(){
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            console.log("selected")
-            res()
-        }, 6000)
-    })
-}
+// function step1(){
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             console.log("selected")
+//             res()
+//         }, 6000)
+//     })
+// }
 
-function step2(){
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            console.log("captionsss")
-            res()
-        }, 3000)
-    })
-}
+// function step2(){
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             console.log("captionsss")
+//             res()
+//         }, 3000)
+//     })
+// }
 
-async function call(){
-    await step1()
-    await step2()
-}
-call()
+// async function call(){
+//     await step1()
+//     await step2()
+// }
+// call()
+
+
+
+fetch("https://jsonplaceholder.typicode.com/todos/").then((data)=>{
+    // console.log(data);
+    return data.json()
+})
+.then((val)=>{
+    console.log(val);
+})
+.catch((err)=>{
+    console.log(errr);
+})
+
