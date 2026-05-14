82a7b8ed314ca4d3ce8fbb9e57d77cee7bdab9f0
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-14 10:41:37 +0530

    async

--- Day6/promisis.js
+++ Day6/promisis.js
@@ -1,4 +1,36 @@
-let promise = new Promise((resolve, reject) => {
+// let promise = new Promise((res, reject) => {
+//     //res()
+//     reject()
 
-})
-console.log(promise)
+// })
+// promise.then(()=>{
+//     console.log("donee");
+// })
+// .catch(()=>{
+//     console.log("error");
+// })
+// console.log(promise)
+
+function step1(){
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("selected")
+            res()
+        }, 6000)
+    })
+}
+
+function step2(){
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("captionsss")
+            res()
+        }, 3000)
+    })
+}
+
+async function call(){
+    await step1()
+    await step2()
+}
+call()
