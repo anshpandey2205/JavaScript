0ca0bca8bde46f3fb5d1ad25d4be5d14e2c4bf85
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-18 10:11:38 +0530

    day7

--- Day6/promisis.js
+++ Day6/promisis.js
@@ -36,15 +36,20 @@
 // call()
 
 
+let h2 = document.querySelector("h2")
 
-fetch("https://jsonplaceholder.typicode.com/todos/").then((data)=>{
+fetch("https://jsonplaceholder.typicode.com/photos/").then((data)=>{
     // console.log(data);
     return data.json()
 })
 .then((val)=>{
     console.log(val);
+
+    val.forEach((item)=>{
+        h2.innerHTML += item.title + "<br><br>"
+    })
 })
 .catch((err)=>{
-    console.log(errr);
+    console.log(err);
 })
 
