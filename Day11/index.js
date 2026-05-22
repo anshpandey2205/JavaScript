4bcfd8b147d76840a070e70ba5a375ee53fb970c
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-22 12:31:06 +0530

    functions

---
+++ Day11/index.js
@@ -0,0 +1,13 @@
+
+
+let str="anshu pandey "
+console.log(str.length);
+console.log(str.toLocaleLowerCase());
+console.log(str.toLocaleUpperCase());
+console.log(str.trim());
+console.log(str.includes("an"));
+console.log(str.indexOf("an"));
+console.log(str.substring(0,5));
+console.log(str.split(" "));
+console.log(str.substr(0,5));
+console.log(str.replace("anshu","ansh"));
