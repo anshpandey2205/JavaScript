a1f3d489e00b52700a99f8251f41699bad8a9da9
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-27 10:44:49 +0530

    react

---
+++ Day12/my-react-app/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+const New = () => {
+  return (
+    <div>new</div>
+  )
+}
+export{New}
+export default Home
+
