748f795424012c697d2556928ea35f2d8aa5707e
anshpandey2205 <anshupandey2227@gmail.com>
2026-06-03 10:02:14 +0530

    todo list

---
+++ Day12/my-react-app/src/ToDo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToDo = () => {
+  return (
+    <div>
+      <h1>ToDo List</h1>
+      <input placeholder="Enter a new task"></input>
+      <button>Add Task</button>
+    </div>
+  )
+}
+
+export default ToDo
